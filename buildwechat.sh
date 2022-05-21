8a9aa7127446e8afbfc855afb0abfab432e5a46e
lin <120377843@qq.com>
2022-05-22 00:33:43 +0800

    feat: add 19.tactical-modeling-principles.md

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
-folder=posts/personal-promotion
-fileName=how-to-use-situation
+folder=column/ddd
+fileName=19.tactical-modeling-principles
 imageHost=imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
