fd017a12b573bd8f74c2751e8b8aac1d48f5edbc
lin <120377843@qq.com>
2022-06-06 08:51:21 +0800

    feat: add new content and change template style

--- buildemail.sh
+++ buildemail.sh
@@ -1,11 +1,12 @@
-folder=_posts/architecture
-fileName=offline-application
-imageHost=imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/main/src/$folder/
-
+folder=posts/business
+fileName=understand-backend-of-software
+imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
 
-cat docs/${folder}/${fileName}.md > build/output.md
-sed -i "" "s@$fileName@$imageHost/$fileName@g" build/output.md
+cat src/${folder}/${fileName}.md > build/output.md
+sed -i "" "s@./$fileName@$imageHost/$fileName@g" build/output.md
 sed -i "" "s@src=$fileName\"@src=\"$imageHost/$fileName@g" build/output.md
 pandoc -s build/output.md -w html --template=template/email_template.html -o build/email.html  --title-prefix email --highlight-style pygments
+
+juice build/email.html build/email.html
