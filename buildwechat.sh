bc715ec34b101d92902518cdc41bd86dc08d8623
lin <120377843@qq.com>
2023-03-20 22:28:19 +0800

    feat: update old content

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
-folder=posts/architecture
-fileName=editable-architecture-diagrams
+folder=posts/tech-lead
+fileName=start-software-project
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
