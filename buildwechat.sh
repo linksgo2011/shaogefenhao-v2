78501e0088f7e5616c0cf0ce33a2576fe06589a9
lin <120377843@qq.com>
2023-04-15 23:34:36 +0800

    feat: add new webinar note

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
 folder=posts/architecture
-fileName=permission-checkpoints
+fileName=data-dictionary-solution
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
