852007ff3858170001e88c88135410f7caf96e54
lin <120377843@qq.com>
2023-06-11 22:11:47 +0800

    post new article

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
 folder=posts/architecture/
-fileName=unified-language
+fileName=fat-and-thin-bff-architecture
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
