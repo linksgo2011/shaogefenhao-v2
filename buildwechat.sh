28556a5c7116db118553f148bd22aaa19db344c0
lin <120377843@qq.com>
2023-06-27 11:43:57 +0800

    update webinar info

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
-folder=posts/team
-fileName=team-consensus-and-technical-decisions
+folder=posts/architecture
+fileName=caching
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
