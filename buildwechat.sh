118f755671aecb0ec66a00b1639c58748fd07f6c
lin <120377843@qq.com>
2023-07-31 22:41:48 +0800

    publish

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
-folder=posts/team/
-fileName=why-ba-and-dev-are-diff
+folder=posts/architecture/
+fileName=real-time-collaboration
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
