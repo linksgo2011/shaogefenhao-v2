170a8f2b338d9a5a90a2315952726d4ba39985b7
linksgo2011 <120377843@qq.com>
2024-03-23 12:32:52 +0800

    update webinar-notes schedule

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
-folder=libs/others
-fileName=cold-medical-knowledge
+folder=libs/ad
+fileName=java-self-testing-ad-wechat
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
@@ -9,4 +9,4 @@ sed -i "" "s@./$fileName@$imageHost/$fileName@g" build/output.md
 sed -i "" "s@src=$fileName\"@src=\"$imageHost/$fileName@g" build/output.md
 pandoc -s build/output.md -w html --template=template/wechat_template.html -o build/wechat.html  --title-prefix email --highlight-style pygments
 
-juice build/wechat.html build/wechat.html
+#juice build/wechat.html build/wechat.html
--- src/libs/data/thoughtworks-apac-data-training.md
+++ src/libs/data/thoughtworks-apac-data-training.md
@@ -7,3 +7,24 @@ date: 2024-03-18 09:18:35
 ## Scale up and scale out
 
 
+
+## Understand Time Windows 
+
+
+## final solution 
+
+Option1: 
+
+Target: 网络热点消退时间周期？
+
+- 怎么定义网络热点：找几个典型的网络事件消退周期，例如：鸭脖事件、
+
+Option2: 空气质量和季节之间的关系？
+
+Option3: 商品销量和假期之间的关系?
+
+Option4: 用户网络购物都在几点？
+
+Option5: 全球灾难之间的关系 Scars of the Earth: Over 22 Million Disasters？
+
+Option5：服装销售和折扣的关系？
