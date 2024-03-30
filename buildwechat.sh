262733d878f1e99d3366ad1c61d35534b54890f6
linksgo2011 <120377843@qq.com>
2024-03-30 17:10:10 +0800

    add new post

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
-folder=libs/ad
-fileName=java-self-self-testing-advertorial
+folder=posts/architecture
+fileName=model-maps
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
@@ -9,4 +9,4 @@ sed -i "" "s@./$fileName@$imageHost/$fileName@g" build/output.md
 sed -i "" "s@src=$fileName\"@src=\"$imageHost/$fileName@g" build/output.md
 pandoc -s build/output.md -w html --template=template/wechat_template.html -o build/wechat.html  --title-prefix email --highlight-style pygments
 
-#juice build/wechat.html build/wechat.html
+juice build/wechat.html build/wechat.html
