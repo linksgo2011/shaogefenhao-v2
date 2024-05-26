074a46ea05cbeca01d7785fb4071be6f4266eb34
linksgo2011 <120377843@qq.com>
2024-05-26 19:08:50 +0800

    add new VI for template

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,4 +1,4 @@
-folder=continue/others-for-wechat-account
+folder=contribute/others-for-wechat-account
 fileName=seckill-system-design
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
@@ -7,6 +7,7 @@ mkdir build
 cat src/${folder}/${fileName}.md > build/output.md
 sed -i "" "s@./$fileName@$imageHost/$fileName@g" build/output.md
 sed -i "" "s@src=$fileName\"@src=\"$imageHost/$fileName@g" build/output.md
+#pandoc -s build/output.md -w html --template=template/wechat_template_2024_new_vi.html -o build/wechat.html  --title-prefix email --highlight-style pygments
 pandoc -s build/output.md -w html --template=template/wechat_template.html -o build/wechat.html  --title-prefix email --highlight-style pygments
 
 juice build/wechat.html build/wechat.html
