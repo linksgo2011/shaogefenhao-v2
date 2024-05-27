83504ff8d5c97020224c72e06d3765d290bc192d
linksgo2011 <120377843@qq.com>
2024-05-27 21:57:06 +0800

    auto commit

--- buildwechat.sh
+++ buildwechat.sh
@@ -7,7 +7,7 @@ mkdir build
 cat src/${folder}/${fileName}.md > build/output.md
 sed -i "" "s@./$fileName@$imageHost/$fileName@g" build/output.md
 sed -i "" "s@src=$fileName\"@src=\"$imageHost/$fileName@g" build/output.md
-#pandoc -s build/output.md -w html --template=template/wechat_template_2024_new_vi.html -o build/wechat.html  --title-prefix email --highlight-style pygments
-pandoc -s build/output.md -w html --template=template/wechat_template.html -o build/wechat.html  --title-prefix email --highlight-style pygments
+pandoc -s build/output.md -w html --template=template/wechat_template_2024_new_vi.html -o build/wechat.html  --title-prefix email --highlight-style pygments
+#pandoc -s build/output.md -w html --template=template/wechat_template.html -o build/wechat.html  --title-prefix email --highlight-style pygments
 
 juice build/wechat.html build/wechat.html
