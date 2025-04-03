a5b098b252a7b5f9897cb1a2a6273deb05b84229
shaogefenhao <shaogefenhao@gmail.com>
2025-04-03 17:55:40 +0800

    add new content

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
 folder=column/tech-lead
-fileName=a-new-understanding-of-management
+fileName=soft-and-hard-approaches
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
