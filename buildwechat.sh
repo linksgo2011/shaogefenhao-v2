44efe5b8cbb596f9534361a3c4828e8f12f75436
shaogefenhao <shaogefenhao@gmail.com>
2025-10-09 22:38:02 +0800

    add contents

--- buildwechat.sh
+++ buildwechat.sh
@@ -1,5 +1,5 @@
-folder=posts/ai
-fileName=better-prompt-for-ai-coding
+folder=column/tech-lead
+fileName=waiting-for-the-conflict
 imageHost=https://raw.githubusercontent.com/linksgo2011/shaogefenhao-v2/master/src/$folder
 rm -rf build
 mkdir build
